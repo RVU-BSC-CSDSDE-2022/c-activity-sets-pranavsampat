247e2dc7dd52dbade53eda0c096dee75edae1f0a
Pranav Pankaj  Pankaj <114479077+pranavsampat@users.noreply.github.com>
2022-12-13 03:44:26 +0000

    ntg

---
+++ set03/problem03.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int input_num(){
+int n;
+printf("enter a number");
+scanf("%d",&n);
+  return n;
+}
+int is_composite(int n){
+  int i,count;
+  count=0;
+  for(i=0;i<n;i++){
+    if(n%(i+1)==0){
+      count=count+1;
+    }
+  }return count;
+  }
+void output(int n,int result){
+  if(result==2){
+    printf("%d is a prime number",n);
+  }
+  else{
+    printf("%d is a composite number",n);
+  }
+}
+int main(){
+  int x,result;
+  x=input_num();
+  result=is_composite(x);
+  output(x,result);
+}
