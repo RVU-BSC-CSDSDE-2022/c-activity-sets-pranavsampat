52d092f70a368076e05468d28e9fa21584f54277
Pranav Pankaj  Pankaj <114479077+pranavsampat@users.noreply.github.com>
2023-01-05 14:41:55 +0000

    .

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,23 +1,29 @@
 #include<stdio.h>
-int main()
-{
-  int a,b;
-  a=input();
-  b=natural_sum(a);
-  output(a,b);
+int input_n();
+int sum_n_nos(int n);
+void output(int n, int sum);
+int input_n(){
+int n;
+printf("enter num");
+scanf("%d",&n);
+return n;
 }
-int input()
-{
-  int n;
-  printf("enter a number");
-  scanf("%d", &n);
-  return n;
-}
-int natural_sum(int n,int sum)
-{
-  sum=(n*(n+1))/2;
+int sum_n_nos(int n){
+int sum;
+  int i;
+  sum=0;
+  for(i=0;i<=n;i++){
+sum=sum+i;
+    }
   return sum;
 }
-void output(int a,int b){
-  printf("the sum of natural numbers %d is %d",a,b);
+void output(int n,int sum){
+printf("Sum is %d",sum);
+}
+int main(){
+int a,sum;
+a=input_n();
+sum=sum_n_nos(a);
+output(a,sum);
+return 0;
 }
