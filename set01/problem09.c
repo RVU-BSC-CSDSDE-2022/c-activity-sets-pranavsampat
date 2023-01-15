c1f8dd5dbc6cab622616950b78941d80f6920e65
Pranav Pankaj  Pankaj <114479077+pranavsampat@users.noreply.github.com>
2023-01-15 10:54:56 +0000

    ...

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,33 +1,30 @@
 #include<stdio.h>
 float input();
-float square_root(float n,float temp, float sqroot);
-void output(float n,float sqroot);
-int main()
-{
-float n,a,sqroot,temp;
-  a=input();
-  temp=0;
-  sqroot=square_root(a,temp,sqroot);
-  output(n,sqroot);
+float square_root(float n);
+void output(float n, float sqrroot);
+
+float input(){
+float n;
+printf("enter number");
+scanf("%f",&n);
+return n;
 }
-  float input()
-{
-  float n;
-  printf("enter a number");
-  scanf("%f",&n);
-  return n;
-  }
-  float square_root(float a,float temp, float sqroot)
-{
-  temp=0;
-  sqroot=a/2;
-  while(sqroot!=temp){
-    temp=sqroot;
-    sqroot=((a/temp)+temp)/2;
-    }
-  return sqroot;
-  }
-void output(float n,float sqroot)
-{
-  printf("the square root is %f",sqroot);
+float square_root(float n){
+float temp=0;
+float sqroot=n/2;
+while(sqroot!=temp){
+  temp=sqroot;
+  sqroot=((n/temp)+temp)/2;
+}
+return sqroot;
+}
+void output(float n,float sqroot){
+printf("square root of %.1f is %.1f",n,sqroot);
+}
+int main(){
+float a,sqroot;
+a=input();
+sqroot=square_root(a);
+output(a,sqroot);
+return 0;
 }
