e911f12e6f6e580d757b6fc83f2576122e92184d
Pranav Pankaj  Pankaj <114479077+pranavsampat@users.noreply.github.com>
2023-01-18 16:01:25 +0000

    ..

---
+++ set05/problem06.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdio.h>
+void input_string(char *a);
+int count_words(char *string);
+void output(char *string, int no_words);
+void input_string(char *a){
+  printf("enter string");
+  scanf("%[^\n]s",a);
+}
+int count_words(char *a){
+  int count;
+  char *token;
+  count=0;
+  token=strtok(a," ");
+  while(token!=NULL){
+    count++;
+    token=strtok(NULL," ");
+  }
+  return count;
+}
+void output(char *a,int no_words){
+  printf("the words in %s is %d\n",a,no_words);
+}
+int main(){
+  char a[100];
+  int x;
+  input_string(a);
+  x=count_words(a);
+  output(a,x);
+}
